33304497c85b01964804debb53b76f712c5390c0
wisdomska <widomska@gmail.com>
2024-07-09 22:45:24 +0000

    Address Booking Application

---
+++ Address_Booking_Application/Form1.cs
@@ -0,0 +1,20 @@
+namespace Address_Booking_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Your details have been saved successfully");
+        }
+    }
+}
